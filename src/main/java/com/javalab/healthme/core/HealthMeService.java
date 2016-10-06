06aa8d1727318e57a840e70f70412161adc8c166
MariaLapovska <maria.lapovsky@gmail.com>
2016-10-06 22:41:30 +0300

    Create tests and methods to count day norm

--- src/main/java/com/javalab/healthme/core/HealthMeService.java
+++ src/main/java/com/javalab/healthme/core/HealthMeService.java
@@ -4,4 +4,36 @@ package com.javalab.healthme.core;
  * @author Mariia Lapovska
  */
 public class HealthMeService {
+
+    public int calculateCaloriesNorm(int weight, int height, int age, Gender
+            gender) {
+        int index = (gender == Gender.MALE) ? 5 : -161;
+
+        return (int) (10 * weight + 6.25 * height - 5 * age + index);
+    }
+
+    public int calculateWaterNorm(int weight, Gender gender) {
+        int index = (gender == Gender.MALE) ? 35 : 31;
+
+        return weight * index;
+    }
+
+    public int calculateStepsNorm(int age, Gender gender) {
+        double index = (gender == Gender.MALE) ? 1.3 : 1.1;
+        int steps;
+
+        if (age < 20) {
+            steps = 15000;
+        } else if (age >= 20 && age <= 50) {
+            steps = 10000;
+        } else {
+            steps = 7000;
+        }
+
+        return (int) (steps * index);
+    }
+
+    public enum Gender {
+        MALE, FEMALE
+    }
 }
--- src/test/java/com/javalab/healthme/core/HealthMeServiceTest.java
+++ src/test/java/com/javalab/healthme/core/HealthMeServiceTest.java
@@ -1,7 +1,36 @@
 package com.javalab.healthme.core;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * @author Mariia Lapovska
  */
 public class HealthMeServiceTest {
+    private HealthMeService healthMeService;
+
+    @Before
+    public void setUp() {
+        healthMeService = new HealthMeService();
+    }
+
+    @Test
+    public void canCountCaloriesNorm() {
+        assertEquals(1370, healthMeService.calculateCaloriesNorm(60, 165, 20,
+                HealthMeService.Gender.FEMALE));
+    }
+
+    @Test
+    public void canCountWaterNorm() {
+        assertEquals(1860, healthMeService.calculateWaterNorm(60,
+                HealthMeService.Gender.FEMALE));
+    }
+
+    @Test
+    public void canCountStepsNorm() {
+        assertEquals(11000, healthMeService.calculateStepsNorm(20,
+                HealthMeService.Gender.FEMALE));
+    }
 }
